8ce99ae0e849d70c46d0092a4a5fea38a8b94daf
silverXz <wanghaoyu00x@gmail.com>
2015-09-13 19:30:29 +0800

    Update Arti3D_VertexLayout.h

--- ToyX/Arti3D_VertexLayout.h
+++ ToyX/Arti3D_VertexLayout.h
@@ -14,19 +14,27 @@ protected:
 
 
 public:
-
+	
+	// Get The Number Of Attributes Of This Vertex.
+	// @param o_iAttribute : Pointer To The Output Variable.
 	Arti3DResult	iGetAttributeNum(uint32_t *o_iAttribute);
+	
+	// Retrieve The Vertex Format Of The Attribute. 
+	// The Result Coulbe Be ARTI3D_FLOAT32, ARTI3D_VECTOR2, etc.
+	// @param i_iAttributeIndex : The Index Of Target Attribute.
+	// @param o_pVertexAttributeFormat : Pointer To The Output Variable.
+	Arti3DResult 	fmtGetVertexAttributeFormate(uint32_t i_iAttributeIndex, Arti3DVertexAttributeFormat *o_pVertexAttributeFormat);
 
-	Arti3DResult fmtGetVertexAttributeFormate(uint32_t i_iAttributeIndex, Arti3DVertexAttributeFormat *o_pVertexAttributeFormat);
-
-	// Get Number Of Float In The Layout.
+	// Retrieve The Total Number Of Float In The Layout.
+	// E.g. VertexLayout : 2 Attributes. Format : {ARTI3D_FLOAT32, ARTI3D_VECTOR4}
+	// The Function Should Return 1 + 4 = 5;
 	uint32_t		iGetFloats();
 
 
 private:
 
-	uint32_t						m_iVertexAttribute;
-	uint32_t						m_iFloats;
+	uint32_t				m_iVertexAttribute;
+	uint32_t				m_iFloats;
 	Arti3DVertexAttributeFormat		*m_pVertexAttributeFormat;
 	
 };
