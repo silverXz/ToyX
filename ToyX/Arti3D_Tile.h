66240f893445c4f3e6bcc02488ea0566dbad7adb
silverXz <wanghaoyu00x@gmail.com>
2015-09-13 16:22:34 +0800

    Update Arti3D_Tile.h

--- ToyX/Arti3D_Tile.h
+++ ToyX/Arti3D_Tile.h
@@ -17,23 +17,38 @@ protected:
 
 
 	Arti3DResult Create();
-
+	
+	// All Triangles Intersecting This Tile Has Been Rasterized!
+	// This Tile Is Ready For Fragment Processing, Set The Flag.
 	void SetReadyForFragmentProcessing();
-
+	
+	// Clear All Information Produced In The Last Frame.
+	// Must Be Called After Or Before Rendering A Frame.
 	void Clear();
 
 
 private:
-	int m_iWidth, m_iHeight;
-	int	m_iX, m_iY;
+	int	m_iWidth;	// Width Of This Tile
+	int 	m_iHeight;	// Height Of This Tile
+	int	m_iX		// Top Left X Coordinate Of This Tile.
+	int 	m_iY;		// Top Left Y Coordinate Of This Tile.
 
-	uint32_t			**m_ppFaceIndexBuffer;
-	uint32_t			*m_pIndexBufferSize;
+	// Face ID Buffer For Every Working Thread. The Buffer Size = threadNum * maxFaceNumPerTile.
+	uint32_t		**m_ppFaceIndexBuffer;
+	// Sizes of Face ID Buffer For Every Working Thread.
+	uint32_t		*m_pIndexBufferSize;
+	// Tile Coverage Type For Very Face In The Face ID Buffer. Its Size = threadNum * maxFaceNumPerTile.
 	Arti3DTileCoverage	**m_ppTileCoverage;
 
+	// Buffer Of Gegenerated Fragments From The Last Stage(Rasterization).
 	std::vector<Arti3DFragment> m_vFragments;
+	
+	// Atomic Flag For Whether This Tile Has Been Added To The Job Queue.
+	// May Be Read/Writen Simutaneously By More Than One Threads, So Its Operation Has To Be Atomic.
 	std::atomic_flag	m_bAddedToJobQueue;
-	bool				m_bFinishedRasterization;
+	
+	// Whether All Rasterization Work Has Been Done.
+	bool			m_bFinishedRasterization;
 };
 
 
